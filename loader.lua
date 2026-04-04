19c05d8a17916fe2019f41203974183a359d78a0
nyztestw <nyztests@gmail.com>
2026-04-04 01:45:02 +0100

    Add files via upload

--- loader.lua
+++ loader.lua
@@ -2,8 +2,8 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 local Games = {
-    [139077630067709] = "https://api.jnkie.com/api/v1/luascripts/public/2b5084dd17a01de1942a8def366f8f381e4c11844901bfa719c45440715fc5ad/download", -- Trap N bang South
-    [106580416109883] = "https://api.jnkie.com/api/v1/luascripts/public/047f4718ca4dc267a4509e0122953aaa307170bb060c063ff5b0c5cc9ffe3978/download", -- Tha Bronx 3 
+    [136676445296302] = "https://api.jnkie.com/api/v1/luascripts/public/2b5084dd17a01de1942a8def366f8f381e4c11844901bfa719c45440715fc5ad/download", -- Trap N bang South
+    [16472538603] = "https://api.jnkie.com/api/v1/luascripts/public/047f4718ca4dc267a4509e0122953aaa307170bb060c063ff5b0c5cc9ffe3978/download", -- Tha Bronx 3 
     [130700367963690] = "https://api.jnkie.com/api/v1/luascripts/public/5d7318b1b03e449b34d38c75435575ca6188e2d85dbcf856bd7b19713292b462/download", -- Philly Streets
 }
 
@@ -22,3 +22,6 @@ if not getgenv().SCRIPT_KEY then
 end
 
 loadstring(game:HttpGet(ScriptURL))()
+
+
+
